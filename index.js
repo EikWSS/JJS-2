869cb58cca21bbd08397f72f441572a96af72cc0
EikWSS <stubbenspille@gmail.com>
2024-12-01 22:57:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,324 @@
+"VIDEO 1"
+
+var myvar = 5
+
+console.log(5 + 5);
+
+console.log(myvar + 10);
+
+console.log(myvar);
+
+console.log(myvar = myvar +10);
+
+console.log(myvar);
+
+console.log(10 - 5);
+
+console.log(myvar - 3 );
+
+console.log(myvar = myvar - 5);
+
+console.log(myvar);
+
+console.log(10 * 10);
+
+console.log(20 / 10);
+
+console.log(myvar = myvar / 2);
+
+console.log(myvar);
+
+console.log(5 + "hello");
+
+console.log(100 + "hello");
+
+console.log("hello" + "david");
+
+console.log("hello" + " david");
+
+console.log(5 * "hello");
+
+
+
+"VIDeo 2"
+var myvar2 = 10
+
+console.log(myvar2 = myvar2 + 5);
+
+console.log(myvar2 += 5);
+
+console.log(myvar2);
+
+console.log(myvar2 -= 10);
+
+console.log(myvar2);
+
+console.log(myvar2 /= 2);
+
+console.log(myvar2);
+
+console.log(myvar2 *= 5);
+
+console.log(myvar2);
+
+console.log(myvar2 += 1);
+
+console.log(myvar2++);
+
+console.log(myvar2);
+
+console.log(++myvar2);
+
+
+"VIDEO 3"
+
+var myvar3 = "eik"
+
+document.write(myvar3)
+
+console.log(myvar3);
+
+document.write(10)
+
+console.log(50)
+
+console.log(20 * 5);
+
+document.write(20 / 5)
+
+
+"VIDEO 4"
+
+var ilikemeat = true  
+
+console.log(ilikemeat);
+
+console.log(ilikemeat = "true");
+
+console.log(ilikemeat);
+
+console.log(ilikemeat = false);
+
+console.log(7 > 5);
+
+console.log(7 < 5);
+
+
+console.log(7 == 7);
+
+console.log(7 == 5);
+
+console.log(0);
+
+Boolean(7 > 5)
+
+Boolean(6)
+
+Boolean(-5)
+
+Boolean(0)
+
+Boolean ("hello")
+
+Boolean ("")
+
+
+"VIDEO 5"
+
+//var youlike = true
+var mynum =8
+
+//if (youlike) {
+   // console.log(youlike);
+   // document.write("you like meat")
+
+    
+    
+//} else {
+    //document.write("you hate meat")
+//}
+
+
+"video 6"
+
+myage = 49
+
+ //if (myage > 30) {
+   // document.write("you are over 30")
+
+//} else if(myage >20) {
+
+//document.write("you are over 20")
+
+//} else if (myage > 10) {
+   // document.write("you are over 10")
+
+//} else {
+   // document.write("you are n0t over 10")
+//}
+
+
+
+
+
+"video 7"
+
+var x = 5
+
+console.log(x > 4);
+
+console.log(x > 5);
+
+console.log(x >= 5);
+
+console.log(x >= 4);
+
+console.log(x < 5);
+
+console.log(x <= 5);
+
+
+console.log(x == 5);
+
+console.log(x === 5);
+
+console.log(x = "5");
+
+console.log(x == 5);
+
+console.log(x === 5);
+
+console.log( x === "5");
+
+console.log(x = 5);
+
+console.log( x != 4);
+
+console.log( x != 5);
+
+console.log(x !== 5);
+
+console.log( x = "5");
+
+console.log(x !== 5);
+
+
+"VIDEO 8"
+
+//var myage = 26
+
+//if (myage1 < 18 || myage1 === 25) {
+
+//document.write("you aint coming")
+
+//} else {
+    
+    //document.write("you can come you cool dude")
+
+//} 
+
+"VIDEO 9"
+//var age = 5
+
+//while ( age < 10) {
+//console.log("you age is less then 10");
+//age++;
+//}
+
+//document.write("your are now over 10");
+
+
+"video 10"
+
+var link = document.getElementsByTagName("a")
+
+for (i = 1; i < links.length; i++) {
+
+    console.log("this is link number" + i);
+age++;
+}
+
+document.write("all lenk now looped")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
